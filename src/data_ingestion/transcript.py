78cbd492bbaee54c7d57df93c2d7941e5a95ca18
Aaron Plahn <aplahn85@gmail.com>
2025-01-09 13:18:41 -0800

    add black formatter and format existing code (#3)

--- src/data_ingestion/transcript.py
+++ src/data_ingestion/transcript.py
@@ -1,39 +1,41 @@
-
 from data_ingestion.audio_label import AudioLabel
 
 
 class Transcript:
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
 
         self.labels = []
 
-    def append(self,label):
+    def append(self, label):
         self.labels.append(label)
-    
-    def fromTsvRows(rows,name):
+
+    def fromTsvRows(rows, name):
         transcript = Transcript(name)
 
         for row in rows:
-            columns = row.split('\t')
+            columns = row.split("\t")
 
             if len(columns) != 3:
                 # TODO should we return errors intead?
-                raise Exception(f"invalid format. expected 3 tab separated values. Received: {row}")
+                raise Exception(
+                    f"invalid format. expected 3 tab separated values. Received: {row}"
+                )
 
-            i,o,t = columns
+            i, o, t = columns
 
-            transcript.append(AudioLabel(in_point=float(i),out_point=float(o),text=t))
+            transcript.append(AudioLabel(in_point=float(i), out_point=float(o), text=t))
 
         return transcript
 
-    '''
+    """
     Return a shallow clone with only labels that satisfy the given predicate function
-    '''
-    def filter(self,predicate):
+    """
+
+    def filter(self, predicate):
         filtered_transcript = Transcript(self.name)
 
-        # why clone if we have a side-effect on self? 
+        # why clone if we have a side-effect on self?
         self.sort()
 
         delta = 0.0
@@ -47,10 +49,8 @@ class Transcript:
                 delta = delta + label.length()
 
         return filtered_transcript
-    
+
     def sort(self):
         get_in_point = lambda label: label.in_point
 
         self.labels.sort(key=get_in_point)
-
-    
